8ff613b72602d99b632d3534e824a5b81c9d20db
mhanggi <29100324+mhanggi@users.noreply.github.com>
2021-04-15 22:33:15 +0200

    Configure yubikey for SSH

--- gpg.nix
+++ gpg.nix
@@ -4,9 +4,12 @@
    services.pcscd.enable = true;
    programs.gnupg.agent.enable = true;
    programs.gnupg.agent.pinentryFlavor = "curses";
+   programs.gnupg.agent.enableSSHSupport = true;
+   programs.ssh.startAgent = false;
    environment.systemPackages = with pkgs; [
      gnupg
    ];
 
+   services.udev.packages = [ pkgs.yubikey-personalization ];
 }
 
