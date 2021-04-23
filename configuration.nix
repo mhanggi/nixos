4654c57f2d5faf3affd6aff7d3455ac57ba1c7a4
mhanggi <29100324+mhanggi@users.noreply.github.com>
2021-04-23 23:29:04 +0200

    Fix incorrect password store

--- configuration.nix
+++ configuration.nix
@@ -436,6 +436,10 @@
 
     programs.password-store = {
       enable = true;
+      settings = {
+        #PASSWORD_STORE_DIR = "$XDG_DATA_HOME/.password-store";
+        PASSWORD_STORE_DIR = "/home/marc/.password-store";
+      };
     };
 
     programs.browserpass = {
