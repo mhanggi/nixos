215a60ab4aafe1328e55fa784b0c1f5151fd5453
mhanggi <29100324+mhanggi@users.noreply.github.com>
2021-08-13 18:55:21 +0200

    Handle locked secrets

--- apply-system.sh
+++ apply-system.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
-pushd ~/config
+
+function areSecretsLocked {
+    git ls-tree -r --name-only -z HEAD secrets/* | xargs -0 grep -qsPa "\x00GITCRYPT"
+}
+
+function unlockSecrets {
+    if areSecretsLocked; then
+        echo "Unlocking secrets"
+        git crypt unlock
+    fi
+}
+
+pushd ~/config > /dev/null
+
+if ! unlockSecrets; then
+    exit 1
+fi
+
 sudo nixos-rebuild switch -I nixos-config=./configuration.nix
-popd
+popd > /dev/null
+
