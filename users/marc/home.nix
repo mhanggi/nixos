eec5c23423b2126f96ccd44994d7912a12de5db0
mhanggi <29100324+mhanggi@users.noreply.github.com>
2021-10-31 19:36:29 +0100

    Launch polybar with i3

--- users/marc/home.nix
+++ users/marc/home.nix
@@ -53,6 +53,8 @@
     anki-bin
     exiftool
     jetbrains.idea-community
+    polybar
+    killall
   ];
 
   gtk = {
@@ -76,9 +78,11 @@
 #        xkb_variant = "altgr-intl";
 #      };
 
-      bars = [{
-        position = "top";
-      }];
+      bars = []; # Empty list removes bar
+
+      startup = [
+        { command = "~/.config/i3/polybar.sh &"; always = true; notification = false; }
+      ];
 
       assigns = {
         "1" = [{ title = "tmux"; }];
@@ -150,6 +154,19 @@
 
     };
   };
+
+  home.file.".config/i3/polybar.sh".text = ''
+    #!/usr/bin/env sh
+
+    # Terminate already running bar instances
+    killall -q polybar
+
+    # Wait until the processes have been shut down
+    while pgrep -x polybar >/dev/null; do sleep 1; done
+
+    # Launch polybar
+    polybar example &
+  '';
 
   wayland.windowManager.sway = {
     enable = true;
